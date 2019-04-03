09735d03fe4cd1c01196e8cc4848d94d876b1af3
Nikita Lebedev <acke208@yandex.ru>
2019-04-03 22:14:09 +0500

    Линия времени вынесена в отдельный компонент

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="ganttChart">
-      <Timeline></Timeline>
+  <div class="timeline">
+      <ul class="timeline__month" v-for="(month, index) in months" :key="index">
+          <span class="month__name">{{ month.name }}</span>
+          <div class="month__days">
+            <li class="day" v-for="day in month.days" :key="day">{{ day }}</li>
+          </div>
+      </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { getTwoMonthsDays, Month } from '@/utils/date';
-import Timeline from '@/components/Timeline.vue';
 
-@Component({
-    components: { Timeline },
-})
-export default class GanttChart extends Vue {
+@Component
+export default class Timeline extends Vue {
     private get months(): Month[] {
         return getTwoMonthsDays(this.startTime.getMonth(), this.startTime.getDate());
     }
@@ -24,20 +26,24 @@ export default class GanttChart extends Vue {
 </script>
 
 <style scoped>
-.ganttChart {
-    max-width: 1300px;
+.timeline {
     display: flex;
 }
 
-.ganttChart__month {
+.timeline__month {
     list-style-type: none;
     display: flex;
     flex-direction: column;
     margin: 0;
-    padding: 5px;
+    padding: 0;
+    border-bottom: 1px solid black;
 }
 
-.ganttChart__month:nth-child(even) {
+.month__name {
+    border-bottom: 1px solid black;
+}
+
+.timeline__month:nth-child(even) {
     border-left: 1px solid black;
     border-right: 1px solid black;
 }
@@ -47,7 +53,7 @@ export default class GanttChart extends Vue {
 }
 
 .day {
-    width: 20px;
+    width: 23px;
     height: 20px;
     text-align: center;
 }
--- src/store/modules/chartCommon.ts
+++ src/store/modules/chartCommon.ts
@@ -3,7 +3,6 @@ import store from '@/store/store';
 
 @Module({ name: 'chartCommon', store, dynamic: true })
 class ChartCommon extends VuexModule {
-    
 }
 
 export const ChartCommonModule = getModule(ChartCommon);
